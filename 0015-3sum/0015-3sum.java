1c17217dd78814230471669ce7b3a4d9bafd7e83
Giwon Nam <44694997+GulliverNam@users.noreply.github.com>
2022-10-22 19:54:30 +0900

    Time: 797 ms (16.21%), Space: 118.7 MB (22.96%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -3,14 +3,12 @@ class Solution {
         Set<List<Integer>> answer = new HashSet<>();
         int length = nums.length;
         Arrays.sort(nums);
-        // System.out.println(Arrays.toString(nums));
         for(int i=0; i<length-2; i++){
             int j = i+1;
             int k = length-1;
             while(j < k){
-                // System.out.println(i+" "+j+" "+k);
-                int sum = nums[j] + nums[k];
-                if(sum + nums[i] == 0){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum == 0){
                     List<Integer> list = new ArrayList<>();
                     list.add(nums[i]);
                     list.add(nums[j]);
@@ -19,7 +17,7 @@ class Solution {
                     answer.add(list);
                     j++;
                     k--;
-                } else if(sum + nums[i] > 0){
+                } else if(sum > 0){
                     k--;
                 } else {
                     j++;
